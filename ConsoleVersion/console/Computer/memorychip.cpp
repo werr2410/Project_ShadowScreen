da46b9f44ab71351d30c51676bf3f732c3ee2e0e
Andrey Horbunov <andrey.horbunov204@gmail.com>
2023-03-14 23:01:06 +0200

    connect memorychip to db

--- ConsoleVersion/console/Computer/memorychip.cpp
+++ ConsoleVersion/console/Computer/memorychip.cpp
@@ -41,19 +41,50 @@ namespace ShadowScreen {
             return "Memorychip";
         }
 
-        void Memorychip::selectDataById(QSqlDatabase &db, int id)
-        {
+        void Memorychip::selectDataById(QSqlDatabase &db, int id) {
+            QSqlQuery query(db);
 
+            setId(id);
+
+            query.prepare("select Manufacturer, Capacity, Description, IsSale, Image, Stars, Status from Memorychip where MemorychipId = :id");
+            query.bindValue(0, id);
+
+            query.exec(); query.next();
+
+            setManufacturer(query.value(0).toString());
+            setCapacity(query.value(1).toString());
+
+            DetailInfo::initFieldDb(query, 2);
         }
 
-        void Memorychip::insertDataTable(QSqlDatabase &db)
-        {
+        void Memorychip::insertDataTable(QSqlDatabase &db) {
+            QSqlQuery query(db);
 
+            query.prepare("exec SmartAddMemorychip :manufacturer, :capacity, :Description, :isSale, :image, :stars, :status, :id");
+            query.bindValue(0, getManufacturer());
+            query.bindValue(1, getCapacity());
+            query.bindValue(2, getCapacity());
+
+            DetailInfo::bindValueDb(query, 3);
+
+            query.bindValue(7, id);
+
+            query.exec();
         }
 
         int Memorychip::getDataById(QSqlDatabase &db)
         {
+            QSqlQuery query(db);
 
+            query.prepare("select MemorychipId from Memorychip where Manufacturer = :manufacturer and Capacity = :Capacity");
+            query.bindValue(0, getManufacturer());
+            query.bindValue(1, getCapacity());
+
+            query.exec(); query.next();
+
+            setId(query.value(0).toInt());
+
+            return id;
         }
 
     }
